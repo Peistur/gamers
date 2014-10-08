2f42c2b45c9044b8f834793bd0cf178ae6730386
Peistur <vpastor85@gmail.com>
2014-10-09 00:44:43 +0200

    environments6

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'gamersmedia',
 			'username'  => 'root',
-			'password'  => 'secret',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
