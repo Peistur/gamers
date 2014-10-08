844dab92ec224d7f73b3802b32c5428b2e68aa46
Peistur <vpastor85@gmail.com>
2014-10-09 00:01:29 +0200

    db config

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => '4gamers',
+			'database'  => 'gamersmedia',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
