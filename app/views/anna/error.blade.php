7bb38fb4ac91b12717bd79f649a3c93759d34555
Peistur <vpastor85@gmail.com>
2015-03-20 23:08:02 +0100

    cumple 2

--- app/views/anna/error.blade.php
+++ app/views/anna/error.blade.php
@@ -2,6 +2,11 @@
 <p>
     Ohhhh, resposta incorrecta! Torna-ho a provar.
 </p>
+
+<p>
+    <img src="http://i104.photobucket.com/albums/m165/fatedreamer/Book%20Blogs/GIFs/wrong_zpshgiuwo4q.gif">
+</p>
+
 <p>
     <a href="{{ URL::action( 'anna' ) }}">Tornar enrera</a>
 </p>
